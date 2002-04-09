bef031aafcfebc3c512e290a0f4ba5704faf4450
Matthew Conway <wr0ngway@users.sourceforge.net>
2002-04-09 16:03:28 +0000

    fully implemented MoveHandler

--- net.sourceforge.eclipseccase/src/net/sourceforge/eclipseccase/MoveHandler.java
+++ net.sourceforge.eclipseccase/src/net/sourceforge/eclipseccase/MoveHandler.java
@@ -1,8 +1,9 @@
 
 package net.sourceforge.eclipseccase;
 
-import java.io.File;
 
+import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.internal.resources.Workspace;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -10,6 +11,7 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.team.IMoveDeleteHook;
 import org.eclipse.core.resources.team.IResourceTree;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -40,7 +42,44 @@ public class MoveHandler implements IMoveDeleteHook
 		int updateFlags,
 		IProgressMonitor monitor)
 	{
-		return false;
+		boolean failed = false;
+		IStatus status = null;
+
+		if ((IResource.FORCE & updateFlags) > 0 &&
+			!tree.isSynchronized(file, IResource.DEPTH_INFINITE))
+		{
+			failed = true;
+			status = new Status(IStatus.ERROR,
+								TeamPlugin.ID,
+								TeamException.UNABLE,
+								"Tree not synchronized",
+								null);
+		}
+		if (!failed && (IResource.KEEP_HISTORY & updateFlags) > 0)
+		{
+			tree.addToLocalHistory(file);
+		}
+		
+		if (! failed)
+		{
+			try
+			{
+				provider.delete(new IResource[] {file}, monitor);
+			}
+			catch(TeamException ex)
+			{
+				failed = true;
+				status = ex.getStatus();
+			}
+		}
+		
+		if (failed)
+			tree.failed(status);
+		else
+			tree.deletedFile(file);
+
+
+		return true;
 	}
 
 	/**
@@ -52,7 +91,45 @@ public class MoveHandler implements IMoveDeleteHook
 		int updateFlags,
 		IProgressMonitor monitor)
 	{
-		return false;
+		boolean failed = false;
+		IStatus status = null;
+
+		if ((IResource.FORCE & updateFlags) > 0 &&
+			!tree.isSynchronized(folder, IResource.DEPTH_INFINITE))
+		{
+			failed = true;
+			status = new Status(IStatus.ERROR,
+								TeamPlugin.ID,
+								TeamException.UNABLE,
+								"Tree not synchronized",
+								null);
+		}
+		if (!failed && (IResource.KEEP_HISTORY & updateFlags) > 0)
+		{
+			// Have to do this recursively for children?
+			//tree.addToLocalHistory(folder);
+		}
+		
+		if (! failed)
+		{
+			try
+			{
+				provider.delete(new IResource[] {folder}, monitor);
+			}
+			catch(TeamException ex)
+			{
+				failed = true;
+				status = ex.getStatus();
+			}
+		}
+		
+		if (failed)
+			tree.failed(status);
+		else
+			tree.deletedFolder(folder);
+
+
+		return true;
 	}
 
 	/**
@@ -64,7 +141,45 @@ public class MoveHandler implements IMoveDeleteHook
 		int updateFlags,
 		IProgressMonitor monitor)
 	{
-		return false;
+		boolean failed = false;
+		IStatus status = null;
+
+		if ((IResource.FORCE & updateFlags) > 0 &&
+			!tree.isSynchronized(project, IResource.DEPTH_INFINITE))
+		{
+			failed = true;
+			status = new Status(IStatus.ERROR,
+								TeamPlugin.ID,
+								TeamException.UNABLE,
+								"Tree not synchronized",
+								null);
+		}
+		// KEEP_HISTORY not needed for project delete
+		
+		// Can't honor NEVER_DELETE_PROJECT_CONTENTS for dynamic views as its
+		// out of our control.  When we handle static views, we will need to honor it
+		
+		
+		if (! failed)
+		{
+			try
+			{
+				provider.delete(new IResource[] {project}, monitor);
+			}
+			catch(TeamException ex)
+			{
+				failed = true;
+				status = ex.getStatus();
+			}
+		}
+		
+		if (failed)
+			tree.failed(status);
+		else
+			tree.deletedProject(project);
+
+
+		return true;
 	}
 
 	/**
@@ -100,7 +215,7 @@ public class MoveHandler implements IMoveDeleteHook
 			status = provider.move(source, destination);
 		}
 		
-		if (failed)
+		if (failed || status.getCode() != IStatus.OK)
 			tree.failed(status);
 		else
 			tree.movedFile(source, destination, tree.computeTimestamp(destination));
@@ -142,7 +257,7 @@ public class MoveHandler implements IMoveDeleteHook
 			status = provider.move(source, destination);
 		}
 
-		if (failed)
+		if (failed || status.getCode() != IStatus.OK)
 			tree.failed(status);
 		else
 			tree.movedFolderSubtree(source, destination);
@@ -161,7 +276,37 @@ public class MoveHandler implements IMoveDeleteHook
 		int updateFlags,
 		IProgressMonitor monitor)
 	{
-		return false;
+		boolean failed = false;
+		IStatus status = null;
+
+		if ((IResource.FORCE & updateFlags) > 0 &&
+			!tree.isSynchronized(source, IResource.DEPTH_INFINITE))
+		{
+			failed = true;
+			status = new Status(IStatus.ERROR,
+								TeamPlugin.ID,
+								TeamException.UNABLE,
+								"Tree not synchronized",
+								null);
+		}
+		if (!failed && (IResource.KEEP_HISTORY & updateFlags) > 0)
+		{
+			// Have to do this recursively for children?
+			//tree.addToLocalHistory(source);
+		}
+		if (! failed)
+		{
+			IResource destination = source.getFolder(description.getLocation());
+			status = provider.move(source, destination);
+		}
+
+		if (failed || status.getCode() != IStatus.OK)
+			tree.failed(status);
+		else
+			tree.movedProjectSubtree(source, description);
+
+
+		return true;
 	}
 
 }
