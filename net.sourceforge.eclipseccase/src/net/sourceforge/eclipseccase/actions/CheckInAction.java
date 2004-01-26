06fb079d65fe8eed38b7c1ad9b3af4c81d38b92d
Matthew Conway <wr0ngway@users.sourceforge.net>
2004-01-26 19:26:32 +0000

    added check for dirty on save

--- net.sourceforge.eclipseccase/src/net/sourceforge/eclipseccase/actions/CheckInAction.java
+++ net.sourceforge.eclipseccase/src/net/sourceforge/eclipseccase/actions/CheckInAction.java
@@ -10,89 +10,111 @@ import net.sourceforge.eclipseccase.ClearcaseProvider;
 import net.sourceforge.eclipseccase.ui.CommentDialog;
 import net.sourceforge.eclipseccase.ui.DirectoryLastComparator;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.team.core.TeamException;
 import org.eclipse.team.internal.ui.actions.TeamAction;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class CheckInAction extends ClearcaseAction
 {
-    /*
-     * Method declared on IActionDelegate.
-     */
-    public void run(IAction action)
-    {
-        String maybeComment = "";
-        int maybeDepth = IResource.DEPTH_ZERO;
 
-        if (ClearcasePlugin.isCheckinComment())
-        {
+
+	/*
+	 * Method declared on IActionDelegate.
+	 */
+	public void run(IAction action)
+	{
+		String maybeComment = "";
+		int maybeDepth = IResource.DEPTH_ZERO;
+		
+		if (ClearcasePlugin.isCheckinComment())
+		{
             CommentDialog dlg = new CommentDialog(shell, "Checkin comment");
-            if (dlg.open() == CommentDialog.CANCEL)
-                return;
+			if (dlg.open() == CommentDialog.CANCEL)
+				return;
             maybeComment = dlg.getComment();
             maybeDepth = dlg.isRecursive() ? IResource.DEPTH_INFINITE : IResource.DEPTH_ZERO;
-        }
+		}
 
-        final String comment = maybeComment;
-        final int depth = maybeDepth;
-        run(new WorkspaceModifyOperation()
-        {
-            public void execute(IProgressMonitor monitor)
-                throws InterruptedException, InvocationTargetException
-            {
-                try
-                {
-                    IResource[] resources = getSelectedResources();
-                    monitor.beginTask("Checking in...", resources.length);
+		final String comment = maybeComment;
+		final int depth = maybeDepth;
+		run(new WorkspaceModifyOperation()
+		{
+			public void execute(IProgressMonitor monitor)
+				throws InterruptedException, InvocationTargetException
+			{
+				try
+				{
+					IResource[] resources = getSelectedResources();
+					monitor.beginTask("Checking in...", resources.length);
 
-                    // Sort resources with directories last so that the modification of a
-                    // directory doesn't abort the modification of files within it.
-                    List resList = Arrays.asList(resources);
-                    Collections.sort(resList, new DirectoryLastComparator());
+					// Sort resources with directories last so that the modification of a
+					// directory doesn't abort the modification of files within it.
+					List resList = Arrays.asList(resources);
+					Collections.sort(resList, new DirectoryLastComparator());
 
-                    for (int i = 0; i < resources.length; i++)
-                    {
-                        IResource resource = resources[i];
-                        IProgressMonitor subMonitor = new SubProgressMonitor(monitor, 1000);
-                        ClearcaseProvider provider = ClearcaseProvider.getProvider(resource);
-                        provider.setComment(comment);
-                        provider.checkin(new IResource[] { resource }, depth, subMonitor);
-                        monitor.worked(1);
-                    }
-                }
-                catch (TeamException e)
-                {
-                    throw new InvocationTargetException(e);
-                }
-                finally
-                {
-                    monitor.done();
-                }
-            }
-        }, "Checkin", TeamAction.PROGRESS_DIALOG);
+					for (int i = 0; i < resources.length; i++)
+					{
+						IResource resource = resources[i];
+						IProgressMonitor subMonitor = new SubProgressMonitor(monitor, 1000);
+						ClearcaseProvider provider = ClearcaseProvider.getProvider(resource);
+						provider.setComment(comment);
+						if (resource.getType() == IResource.FILE)
+						{	
+							IEditorPart editor = ClearcasePlugin.getDefault().getWorkbench()
+								.getActiveWorkbenchWindow()
+								.getActivePage().findEditor(new FileEditorInput((IFile) resource));
+							if (editor != null && editor.isDirty())
+							{
+								boolean answer = MessageDialog.openQuestion(shell, "Resource dirty", "The resource " + resource.toString() + " is dirty, do you wish to save before checking in?");
+								if (answer)
+								{
+									editor.doSave(null);
+								}
 
-        updateActionEnablement();
-    }
+							}
+						}
+						provider.checkin(new IResource[] {resource},
+										depth, subMonitor);
+						monitor.worked(1);
+					}
+				}
+				catch (TeamException e)
+				{
+					throw new InvocationTargetException(e);
+				}
+				finally
+				{
+					monitor.done();
+				}
+			}
+		}, "Checkin", TeamAction.PROGRESS_DIALOG);
 
-    protected boolean isEnabled()
-    {
-        IResource[] resources = getSelectedResources();
-        if (resources.length == 0)
-            return false;
-        for (int i = 0; i < resources.length; i++)
-        {
-            IResource resource = resources[i];
-            ClearcaseProvider provider = ClearcaseProvider.getProvider(resource);
+		updateActionEnablement();
+	}
+
+	protected boolean isEnabled()
+	{
+		IResource[] resources = getSelectedResources();
+		if (resources.length == 0)
+			return false;
+		for (int i = 0; i < resources.length; i++)
+		{
+			IResource resource = resources[i];
+			ClearcaseProvider provider = ClearcaseProvider.getProvider(resource);
             if (provider == null || provider.isUnknownState(resource) || provider.isIgnored(resource))
-                return false;
-            if (!provider.isCheckedOut(resource))
-                return false;
-        }
-        return true;
-    }
+				return false;
+			if (! provider.isCheckedOut(resource))
+				return false;
+		}
+		return true;
+	}
 
 }
