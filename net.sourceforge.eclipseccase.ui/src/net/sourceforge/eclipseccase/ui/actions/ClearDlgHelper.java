ba612bc216715d4f66892d4db2d91dd5c87baffb
Gunnar Wagenknecht <gwagenknecht@users.sourceforge.net>
2005-11-10 13:52:17 +0000

    fixed 1293016 - Automatic checkout on edit causes file to go out sync

--- net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/ClearDlgHelper.java
+++ net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/ClearDlgHelper.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.clearcase.simple.ClearDlg;
+import net.sourceforge.eclipseccase.ClearcasePlugin;
 import net.sourceforge.eclipseccase.ClearcaseProvider;
 
 import org.eclipse.core.resources.IResource;
@@ -29,111 +30,124 @@ import org.eclipse.team.core.TeamException;
  */
 public class ClearDlgHelper {
 
-    private static final String _UNCHECKOUT = "/uncheckout"; //$NON-NLS-1$
+	/**
+	 * "cleardlg" command line argument for preserving the time;
+	 */
+	private static final String PTIME = "/ptime"; //$NON-NLS-1$
 
-    private static final String _CHECKOUT = "/checkout"; //$NON-NLS-1$
+	private static final String UNCHECKOUT = "/uncheckout"; //$NON-NLS-1$
 
-    private static final String _CHECKIN = "/checkin"; //$NON-NLS-1$
+	private static final String CHECKOUT = "/checkout"; //$NON-NLS-1$
 
-    private static final String _ADDTOSRC = "/addtosrc"; //$NON-NLS-1$
+	private static final String CHECKIN = "/checkin"; //$NON-NLS-1$
 
-    /**
-     * Adds the specified resources.
-     * 
-     * @param resources
-     * @throws TeamException
-     */
-    public static void add(IResource[] resources) throws TeamException {
-        // execute cleardlg
-        perform(_ADDTOSRC, resources);
-    }
+	private static final String ADDTOSRC = "/addtosrc"; //$NON-NLS-1$
 
-    /**
-     * Checkin the specified resources.
-     * 
-     * @param resources
-     * @throws TeamException
-     */
-    public static void checkin(IResource[] resources) throws TeamException {
-        // execute cleardlg
-        perform(_CHECKIN, resources);
-    }
+	/**
+	 * Adds the specified resources.
+	 * 
+	 * @param resources
+	 * @throws TeamException
+	 */
+	public static void add(IResource[] resources) throws TeamException {
+		// execute cleardlg
+		perform(ADDTOSRC, resources);
+	}
 
-    /**
-     * Checkout the specified resources.
-     * 
-     * @param resources
-     * @throws TeamException
-     */
-    public static void checkout(IResource[] resources) throws TeamException {
-        // execute cleardlg
-        perform(_CHECKOUT, resources);
-    }
+	/**
+	 * Checkin the specified resources.
+	 * 
+	 * @param resources
+	 * @throws TeamException
+	 */
+	public static void checkin(IResource[] resources) throws TeamException {
+		// execute cleardlg
+		perform(CHECKIN, resources);
+	}
 
-    /**
-     * Uncheckout the specified resources.
-     * 
-     * @param resources
-     * @throws TeamException
-     */
-    public static void uncheckout(IResource[] resources) throws TeamException {
-        // execute cleardlg
-        perform(_UNCHECKOUT, resources);
-    }
+	/**
+	 * Checkout the specified resources.
+	 * 
+	 * @param resources
+	 * @throws TeamException
+	 */
+	public static void checkout(IResource[] resources) throws TeamException {
+		// execute cleardlg
+		perform(CHECKOUT, resources);
+	}
 
-    /**
-     * Adds the specified resources.
-     * 
-     * @param operation
-     * @param resources
-     * @throws TeamException
-     */
-    public static void perform(String operation, IResource[] resources)
-            throws TeamException {
-        // prepare command
-        List command = new ArrayList(1 + resources.length);
-        command.add(operation);
+	/**
+	 * Uncheckout the specified resources.
+	 * 
+	 * @param resources
+	 * @throws TeamException
+	 */
+	public static void uncheckout(IResource[] resources) throws TeamException {
+		// execute cleardlg
+		perform(UNCHECKOUT, resources);
+	}
 
-        // append files
-        for (int i = 0; i < resources.length; i++) {
-            IResource resource = resources[i];
-            command.add(resource.getLocation().toOSString());
-        }
+	/**
+	 * Adds the specified resources.
+	 * 
+	 * @param operation
+	 * @param resources
+	 * @throws TeamException
+	 */
+	public static void perform(String operation, IResource[] resources)
+			throws TeamException {
+		// prepare command
+		List command = new ArrayList(1 + resources.length);
+		command.add(operation);
 
-        // execute cleardlg
-        ClearDlg clearDlg = new ClearDlg((String[]) command
-                .toArray(new String[command.size()]));
-        try {
-            int success = clearDlg.execute();
-            if (0 != success && success != resources.length)
-                    throw new TeamException("Execution of cleardlg "
-                            + operation + " failed (process returned: "
-                            + clearDlg.getExitValue() + ")!");
-        } catch (IOException e) {
-            throw new TeamException("Execution of cleardlg " + operation
-                    + " failed: " + e.getMessage(), e);
-        }
+		// append files
+		for (int i = 0; i < resources.length; i++) {
+			IResource resource = resources[i];
+			command.add(resource.getLocation().toOSString());
+		}
 
-        // refresh resources
-        for (int i = 0; i < resources.length; i++) {
-            IResource resource = resources[i];
-            ClearcaseProvider provider = ClearcaseProvider
-                    .getClearcaseProvider(resource);
+		// preserver time if enabled and allowed
+		if (ClearcasePlugin.isPreserveTimes()
+				&& (CHECKOUT.equalsIgnoreCase(operation)
+						|| CHECKIN.equalsIgnoreCase(operation) || ADDTOSRC
+						.equalsIgnoreCase(operation))) {
+			command.add(PTIME);
+		}
 
-            // refresh resource
-            provider.refresh(resource);
+		// execute cleardlg
+		ClearDlg clearDlg = new ClearDlg((String[]) command
+				.toArray(new String[command.size()]));
+		try {
+			int success = clearDlg.execute();
+			if (0 != success && success != resources.length)
+				throw new TeamException("Execution of cleardlg " + operation
+						+ " failed (process returned: "
+						+ clearDlg.getExitValue() + ")!");
+		} catch (IOException e) {
+			throw new TeamException("Execution of cleardlg " + operation
+					+ " failed: " + e.getMessage(), e);
+		}
 
-            // also invalidate state of parent container
-            // (some operations my checkout parent)
-            provider.refresh(resource.getParent());
-        }
-    }
+		// refresh resources
+		for (int i = 0; i < resources.length; i++) {
+			IResource resource = resources[i];
+			ClearcaseProvider provider = ClearcaseProvider
+					.getClearcaseProvider(resource);
 
-    /**
-     * No instance necessary.
-     */
-    private ClearDlgHelper() {
-        super();
-    }
+			// refresh resource
+			provider.refresh(resource);
+
+			// also invalidate state of parent container
+			// (some operations my checkout parent)
+			provider.refresh(resource.getParent());
+		}
+	}
+
+	/**
+	 * No instance necessary.
+	 */
+	private ClearDlgHelper() {
+		super();
+	}
 
 }
