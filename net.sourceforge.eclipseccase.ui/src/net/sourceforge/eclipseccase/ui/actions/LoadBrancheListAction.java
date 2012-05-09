acdfebd3415fc03c077a4961d5bbdc684883f77a
Mattias Rundgren <pmattru@users.sourceforge.net>
2012-05-09 16:07:06 +0200

    Added support for linked folders in feature search file on branch.

--- net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/LoadBrancheListAction.java
+++ net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/LoadBrancheListAction.java
@@ -1,5 +1,12 @@
 package net.sourceforge.eclipseccase.ui.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.eclipse.core.resources.IResource;
+
 import net.sourceforge.eclipseccase.views.BranchSearchView;
 
 import org.eclipse.core.resources.IProject;
@@ -43,17 +50,31 @@ public class LoadBrancheListAction  extends ClearCaseWorkspaceAction {
 			public void run(IProgressMonitor monitor) throws CoreException {
 
 				try {
+					// TODO Add code below to loop over all linked folders (MR)
 					if (project != null) {
 						String []branches;
-						
-						File workingDir = new File(project.getLocation().toOSString());
-						
 						ClearCaseProvider p = ClearCaseProvider.getClearCaseProvider(project);
-						if (p != null && (p.isClearCaseElement(project)))
+						Set<File> folders = getProjectsAllFolders(p); // Where to look
+						Set<String> branchesUnique = new HashSet<String>();
+						for(File vob: folders)
 						{
-							branches = p.loadBrancheList(workingDir);
-							view.setBranches(branches);
-						}
+							branches = p.loadBrancheList(vob);
+							for(String r : branches)
+							{
+								if(branchesUnique.add(r)) // Only adds if not added before
+								{
+//									System.out.format("Added branch %s \tfrom path %s\n", r, vob.getAbsolutePath());
+								}
+							}
+						}						
+//						File workingDir = new File(project.getLocation().toOSString());
+						view.setBranches(branchesUnique.toArray(new String[0]));
+						
+//						if (p != null && (p.isClearCaseElement(project)))
+//						{
+//							branches = p.loadBrancheList(workingDir);
+//							view.setBranches(branches);
+//						}
 					}
 				} finally {
 					monitor.done();
@@ -62,5 +83,39 @@ public class LoadBrancheListAction  extends ClearCaseWorkspaceAction {
 		};
 
 		executeInBackground(runnable, "Load branch list");
+	}
+
+	private Set<File> getProjectsAllFolders(ClearCaseProvider p) throws CoreException {
+		// TODO Return only one folder in each VOB.
+		// Find all folders
+		Set<File> folders = new HashSet<File>();
+		IResource [] projMembers = project.members();
+		if (p.isClearCaseElement(project))
+		{
+			folders.add(project.getLocation().toFile());
+		}
+		for(IResource projMem : projMembers)
+		{
+			if((projMem.getType() == IResource.FOLDER) &&
+					p.isClearCaseElement(projMem))
+			{
+				if(projMem.isLinked())
+				{
+					folders.add(projMem.getLocation().toFile());
+				}
+				else
+				{
+					String symbolicLink = p.getSymbolicLinkTarget(projMem);
+					String location = projMem.getLocation().toOSString();
+					if(symbolicLink.length() > 0)
+					{
+						location = projMem.getParent().getLocation().toOSString();
+						File path = new File(location,symbolicLink);
+						folders.add(path);
+					}
+				}
+			}
+		}
+		return folders;
 	}
 }
