857cd3b2177c5bcc29e979f098f518043dc54ace
mike <mikaelpetterson@hotmail.com>
2012-06-08 15:40:14 +0200

    Updated test after refactoring

--- net.sourceforge.eclipseccase.ui.test/src/net/sourceforge/eclipseccase/ui/test/DiffMergePreferencesTest.java
+++ net.sourceforge.eclipseccase.ui.test/src/net/sourceforge/eclipseccase/ui/test/DiffMergePreferencesTest.java
@@ -2,13 +2,14 @@ package net.sourceforge.eclipseccase.ui.test;
 
 
 
-import java.util.HashMap;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import junit.framework.Assert;
 
-import net.sourceforge.eclipseccase.ui.preferences.DiffMergePreferencePage;
+import net.sourceforge.eclipseccase.diff.PreferenceHelper;
+
 
 import org.junit.Test;
 
@@ -25,7 +26,7 @@ public class DiffMergePreferencesTest {
 	@Test
 	public void testStrToMap() {
 		String preferenceValue = KDIFF3+":"+KDIFF3_PATH+";"+CLEARCASE+":"+CLEARCASE_PATH+";";
-		Map<String,String> map = DiffMergePreferencePage.strToMap(preferenceValue);
+		Map<String,String> map = PreferenceHelper.strToMap(preferenceValue);
 		Assert.assertFalse("Map shouldn't have been empty",
 	                       map.isEmpty());
 		Assert.assertEquals(KDIFF3_PATH, map.get(KDIFF3));
@@ -37,7 +38,7 @@ public class DiffMergePreferencesTest {
 		Map<String,String> map = new LinkedHashMap<String,String>();
 		map.put(KDIFF3,KDIFF3_PATH);
 		map.put(CLEARCASE,CLEARCASE_PATH);
-		String actual = DiffMergePreferencePage.mapToStr(map);
+		String actual = PreferenceHelper.mapToStr(map);
 		Assert.assertEquals(expected, actual);
 		
 	}
