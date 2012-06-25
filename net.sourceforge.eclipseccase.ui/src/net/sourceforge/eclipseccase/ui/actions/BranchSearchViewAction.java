a392b5d52835fa0f2d13ed4bc3d5b116674b74ee
mike <mikaelpetterson@hotmail.com>
2012-06-25 10:16:49 +0200

    Old historyview has been removed and import statement removed.

--- net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/BranchSearchViewAction.java
+++ net.sourceforge.eclipseccase.ui/src/net/sourceforge/eclipseccase/ui/actions/BranchSearchViewAction.java
@@ -1,17 +1,8 @@
 package net.sourceforge.eclipseccase.ui.actions;
 
-import net.sourceforge.eclipseccase.views.BranchSearchView;
-
-import net.sourceforge.eclipseccase.ClearCasePlugin;
-
-import java.util.Vector;
-import net.sourceforge.clearcase.*;
 import net.sourceforge.eclipseccase.ClearCaseProvider;
-import net.sourceforge.eclipseccase.views.HistoryView;
+import net.sourceforge.eclipseccase.views.BranchSearchView;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.PlatformUI;
@@ -47,18 +38,16 @@ public class BranchSearchViewAction extends ClearCaseWorkspaceAction {
 		resources = getSelectedResources();
 		if (resources.length == 0)
 			return;
-		
+
 		ClearCaseProvider provider = ClearCaseProvider.getClearCaseProvider(resources[0].getProject());
-		if (provider == null) {
+		if (provider == null)
 			return;
-		}
-		
+
 		try {
-			view = (BranchSearchView) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-						.getActivePage().showView("net.sourceforge.eclipseccase.views.BranchSearchView");
-		
-		    view.setProject(resources[0].getProject());
-		
+			view = (BranchSearchView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("net.sourceforge.eclipseccase.views.BranchSearchView");
+
+			view.setProject(resources[0].getProject());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
